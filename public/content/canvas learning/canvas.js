374d31689aef58f13ee98cc3ea83041291fd1f30
clash-shubh <shubhambhawsar63@gmail.com>
2020-10-31 23:22:04 +0530

    added a concept Collision detection

--- public/content/canvas learning/canvas.js
+++ public/content/canvas learning/canvas.js
@@ -33,7 +33,7 @@ function Circle(x,y,dx,dy,radius) {
   this.y=y;
   this.dx = dx;
   this.dy = dy;
-  this.radius = Math.random() * 5 + 1;
+  this.radius = 20;
   this.min=this.radius;
   this.color= colorArray[Math.floor(Math.random() * colorArray.length)];
 
@@ -41,13 +41,34 @@ function Circle(x,y,dx,dy,radius) {
   this.draw = function() {
 
     c.beginPath();
-    c.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
+    c.arc(this.x,this.y,this.radius,0,Math.PI*2,true);
     c.fillStyle = this.color;
 
     c.fill();
   }
+
+  this.Collision = function() {
+    for(var i=0;i<50;i++){
+      if(this.x!=circleArray[i].x && this.y!=circleArray[i].y && this.dx!=circleArray[i].dx && this.dy!=circleArray[i].dy){
+        distance_x=circleArray[i].x - this.x;
+        distance_y=circleArray[i].y - this.y;
+        distance=distance_x*distance_x + distance_y * distance_y;
+        radius_sum=(circleArray[i].radius+this.radius) * (circleArray[i].radius+this.radius);
+      //console.log("distance = "+distance);
+      if(distance<=radius_sum && distance!=NaN){
+        
+        this.dx=-this.dx;
+        this.dy=-this.dy;
+        circleArray[i].dx=-circleArray[i].dx;
+        circleArray[i].dy=-circleArray[i].dy;
+      }
+    }
+  }
+  }
   
   this.update = function() {
+    this.Collision();
+
     this.x+=this.dx;
     this.y+=this.dy;
 
@@ -57,24 +78,26 @@ function Circle(x,y,dx,dy,radius) {
     if(this.y+this.radius>innerHeight || this.y-this.radius<0)
     this.dy=-this.dy;
 
+    
+    /*
     if(mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50 ){
       if(this.radius < maxRadius) {this.radius+=1};
 
     }else if(this.radius > this.min) {
         this.radius-=1;
     }
-
+    */
     this.draw();
   }
+
+
+
 }
 
 var circleArray = [];
 
 function init(){
-
-
-circleArray = [];
-for(var i=0;i<1000;i++){
+for(var i=0;i<50;i++){
     var x=Math.random() * (window.innerWidth - radius*2) + radius;
     var y=Math.random() * (window.innerHeight - radius*2) + radius;
     var dx=(Math.random()-0.5)*4;
